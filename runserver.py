644139af4e50b2ba7abfceb4b91be0cdccc6d915
Bryan Helmig <bryan@bryanhelmig.com>
2012-03-21 00:23:53 -0500

    more production prep

--- runserver.py
+++ runserver.py
@@ -2,14 +2,7 @@ from gevent import monkey
 monkey.patch_all()
 
 import gevent.wsgi
-import werkzeug.serving
-
 from emailpie import app
 
-
-@werkzeug.serving.run_with_reloader
-def runServer():
-    ws = gevent.wsgi.WSGIServer(('', 24259), app)
-    ws.serve_forever()
-
-runServer()
+ws = gevent.wsgi.WSGIServer(('', 24259), app)
+ws.serve_forever()
